579a3177750b5141910a953f062ee0b2841d688d
Rigo8 <rigoah.8@live.com>
2016-09-23 17:05:59 -0300

    Update Contador.java
    Se agrego constructor, atributo salario y los metodos getSalario y setSalario

--- demo/Contador.java
+++ demo/Contador.java
@@ -1,5 +1,20 @@
 package demo;
 
 public class Contador extends Empleado {
+  
+  public Contador(){
+    
+    }
+
+    @Override
+    public void setSalario(double salario) {
+        super.setSalario(salario); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getSalario() {
+        salario=(super.getSalario()*.35)+super.getSalario();
+        return salario; //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
