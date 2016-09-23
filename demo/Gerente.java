cc635dcdd9d0977efcfa0be0774d63975b769695
Rigo8 <rigoah.8@live.com>
2016-09-23 17:07:48 -0300

    Update Gerente.java
    Se agrego el atributo salario, el metodo constructor para la clase Gerente y los metodos getSalario y setSalario

--- demo/Gerente.java
+++ demo/Gerente.java
@@ -1,5 +1,22 @@
 package demo;
 
 public class Gerente extends Empleado {
+  
+  private  double salario;
+  public Gerente(){
+
+}
+
+    @Override
+    public void setSalario(double salario) {
+               
+        super.setSalario(salario); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getSalario() {
+        salario=(super.getSalario()*.50)+super.getSalario();
+        return this.salario; //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
